5c66b04ff0191784848e4a36f0ed3096ece84bbd
Jonas Auer <jonas.auer.94@gmail.com>
2021-09-21 00:25:58 +0200

    (frontend) Add support for className prop to Time component

--- packages/frontend/src/components/time.tsx
+++ packages/frontend/src/components/time.tsx
@@ -4,22 +4,32 @@ export interface TimeProps {
   time: Date | string | Dayjs
   format?: string
   asLink?: boolean
+  className?: string
 }
 export function Time({
   time,
   format = 'llll',
   asLink = false,
+  className,
 }: TimeProps): JSX.Element {
   const t = dayjs(time)
 
   if (asLink) {
     const nakamuraLink = `https://time.nakamura-labs.com/#${t.unix()}`
     return (
-      <a href={nakamuraLink} target="_blank" rel="noreferrer">{t.format(format)}</a>
+      <a
+        className={className}
+        href={nakamuraLink}
+        target="_blank"
+        rel="noreferrer"
+        onClick={e => e.stopPropagation()}
+      >
+        {t.format(format)}
+      </a>
     )
   }
 
   return (
-    <span>{t.format(format)}</span>
+    <span className={className}>{t.format(format)}</span>
   )
 }
