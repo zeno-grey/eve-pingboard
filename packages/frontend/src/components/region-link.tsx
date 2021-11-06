d720a42981dbf2067a39deb398adec7fd8838f8d
Jonas Auer <jonas.auer.94@gmail.com>
2021-11-06 14:05:28 +0100

    (frontend) Fix Evemaps links being invalid when their region or system name contains spaces

--- packages/frontend/src/components/region-link.tsx
+++ packages/frontend/src/components/region-link.tsx
@@ -1,6 +1,6 @@
 export function RegionLink(props: { region: string, system?: string }): JSX.Element {
-  const link = `https://evemaps.dotlan.net/map/${props.region}` +
-    (props.system ? `/${props.system}` : '')
+  const link = `https://evemaps.dotlan.net/map/${props.region.replaceAll(' ', '_')}` +
+    (props.system ? `/${props.system.replaceAll(' ', '_')}` : '')
 
   return (
     <a href={link} target="_blank" rel="noreferrer">
--- packages/frontend/src/components/solar-system-link.tsx
+++ packages/frontend/src/components/solar-system-link.tsx
@@ -1,5 +1,5 @@
 export function SolarSystemLink(props: { system: string }): JSX.Element {
-  const link = `https://evemaps.dotlan.net/system/${props.system}`
+  const link = `https://evemaps.dotlan.net/system/${props.system.replaceAll(' ', '_')}`
   return (
     <a href={link} target="_blank" rel="noreferrer">
       {props.system}
