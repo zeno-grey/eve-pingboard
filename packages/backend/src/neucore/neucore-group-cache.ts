05f63dd9766c7fcd7736b3721a1c76295a635208
Jonas Auer <jonas.auer.94@gmail.com>
2022-07-25 22:06:11 +0200

    Retry failed Neucore requests up to 5 times
    Ref #65

--- packages/backend/src/neucore/neucore-group-cache.ts
+++ packages/backend/src/neucore/neucore-group-cache.ts
@@ -1,3 +1,4 @@
+import { handleAll, retry, ExponentialBackoff } from 'cockatiel'
 import { InMemoryTTLCache } from '../util/in-memory-ttl-cache'
 import { NeucoreGroupsProvider } from '../middleware/user-roles'
 import { NeucoreClient } from './neucore-client'
@@ -9,11 +10,20 @@ export class NeucoreGroupCache implements NeucoreGroupsProvider {
     neucoreClient: NeucoreClient
     cacheTTL: number
   }) {
+    const policy = retry(handleAll, {
+      backoff: new ExponentialBackoff(),
+      maxAttempts: 5,
+    })
+
     this.#cache = new InMemoryTTLCache({
       defaultTTL: options.cacheTTL,
       get: async characterId => {
         console.log('getting neucore groups for', characterId)
-        const groups = await options.neucoreClient.getCharacterGroups(characterId)
+
+        const groups = await policy.execute(() =>
+          options.neucoreClient.getCharacterGroups(characterId)
+        )
+
         return { value: groups.map(g => g.name) }
       },
     })
