98dac750f5985c48c29b869f15b3b5e14622773f
Tian Khamez <tkhamez@gmail.com>
2022-12-20 13:50:50 +0100

    Remove footer from login page

--- packages/frontend/src/components/app-footer.tsx
+++ packages/frontend/src/components/app-footer.tsx
@@ -5,10 +5,6 @@ export function AppFooter(): JSX.Element {
   return (
     <Navbar variant="dark" bg="primary" expand="lg">
       <Container>
-        <div className="footer">
-          Brave Collective Services. For support write to support@bravecollective.freshdesk.com
-          or ask in the ingame channel &apos;Brave IT Team&apos;.
-        </div>
       </Container>
     </Navbar>
   )
